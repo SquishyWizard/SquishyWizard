7b8792ff803efcd49d0f57d2452f17c1a428b4ea
SquishyWizard <89790693+SquishyWizard@users.noreply.github.com>
2022-03-21 11:48:53 -0400

    Update reverseCapsLock.py

--- reverseCapsLock.py
+++ reverseCapsLock.py
@@ -1,3 +1,9 @@
+#This is a small simple program I created for myself. 
+#I use caps lock when typing long lines of text and on occasions 
+#finding myself in the predicament of a reverse caps lock situation. 
+
+#This was my quick solution to the issue. 
+
 inputText = input("Enter your wrongdoing: ")
 for element in inputText:
   if element.isupper() == True:
