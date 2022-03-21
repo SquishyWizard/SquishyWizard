0fb5df8c9de1421c49b5b5905dbde166b0707d70
SquishyWizard <89790693+SquishyWizard@users.noreply.github.com>
2022-03-21 11:50:21 -0400

    Update reverseCapsLock.py

--- reverseCapsLock.py
+++ reverseCapsLock.py
@@ -1,6 +1,6 @@
-#This is a small simple program I created for myself. 
-#I use caps lock when typing long lines of text and on occasions 
-#finding myself in the predicament of a reverse caps lock situation. 
+# This is a small simple program I created for myself. 
+# I use caps lock when typing long lines of text and on occasions 
+# finding myself in the predicament of a reverse caps lock situation. 
 
 #This was my quick solution to the issue. 
 
